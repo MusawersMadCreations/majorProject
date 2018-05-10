561ef3b41c5f40ae41319899abcd6d81e20f88cd
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-05-10 15:20:22 -0600

    www

--- roboto incryptor/sketch.js
+++ roboto incryptor/sketch.js
@@ -5,17 +5,22 @@
 
 // global variables
 let aSound,bSound,cSound,dSound,eSound,fSound,gSound,hSound,iSound,jSound,kSound,lSound,mSound,nSound;
-let oSound,pSound,qSound,rSound,sSound,tSound,uSound,vSound,wound,xSound,ySound,zSound,spaceSound;
+let oSound,pSound,qSound,rSound,sSound,tSound,uSound,vSound,wSound,xSound,ySound,zSound,spaceSound;
 
 
 function preload() {
-  //ALL SOUNDS FOR EVERY LETTER
+  aSound = loadSound("sounds/A.wav"),bSound = loadSound("sounds/B.wav"),cSound = loadSound("sounds/C.wav"),dSound = loadSound("sounds/D.wav"),eSound = loadSound("sounds/E.wav");
+  fSound = loadSound("sounds/F.wav"),gSound = loadSound("sounds/G.wav"),hSound = loadSound("sounds/H.wav"),iSound = loadSound("sounds/I.wav"),jSound = loadSound("sounds/J.wav");
+  kSound = loadSound("sounds/K.wav"),lSound = loadSound("sounds/L.wav"),mSound = loadSound("sounds/M.wav"),nSound = loadSound("sounds/N.wav"),oSound = loadSound("sounds/O.wav");
+  pSound = loadSound("sounds/P.wav"),qSound = loadSound("sounds/Q.wav"),rSound = loadSound("sounds/R.wav"),sSound = loadSound("sounds/S.wav"),tSound = loadSound("sounds/T.wav");
+  uSound = loadSound("sounds/U.wav"),vSound = loadSound("sounds/V.wav"),wSound = loadSound("sounds/W.wav"),xSound = loadSound("sounds/X.wav"),ySound = loadSound("sounds/Y.wav");
+  zSound = loadSound("sounds/Z.wav");
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  aSound = loadSound("sounds/A.wav"); 
+
 }
 
 function draw() {
