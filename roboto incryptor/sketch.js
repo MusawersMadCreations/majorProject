de5be2636b5aa17631e43f84b1c8a63a874f8621
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-05-22 15:21:21 -0600

    dsadsad

--- roboto incryptor/sketch.js
+++ roboto incryptor/sketch.js
@@ -21,17 +21,22 @@ function preload() {
   zSound = loadSound("sounds/Z.wav");
 }
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fSound.play();
   // creates text input box
   textbox = createInput();
   textbox.position(700, 500);
-  textbox.input(recordInput);   // records text
 
-  submitButton = createButton("submit");
+
+  submitButton = createButton("Submit");
+  playSoundButton = createButton("Incrypt");
   submitButton.position(textbox.x + textbox.width, 500);
+
+  changeTextToSound();
+  textbox.changed(recordInput);   // records text
+
+
 }
 
 function draw() {
@@ -40,8 +45,12 @@ function draw() {
 }
 
 function recordInput(){
-  lettersList.push(textbox.value().split(""));
-  console.log(textbox.value());
+  lettersList = [];
+  soundsList = [];
+  lettersList = textbox.value().split("");
+  console.log(lettersList);
+  changeTextToSound();
+  playSoundButton.mousePressed(playsound);
 }
 
 function makePlayButton(){
@@ -52,9 +61,88 @@ function makePlayButton(){
 }
 
 function changeTextToSound(){
-  for (let i = 0; i < lettersList[0].length; i++) {
-    if (lettersList[i] === "a"){
-      soundsList.push("OMG");
+  for (let i = 0; i < lettersList.length; i++) {
+    if (lettersList[i] === "a" || lettersList[i] === "A"){
+      soundsList.push(aSound);
+    }
+    if (lettersList[i] === "b"|| lettersList[i] === "B"){
+      soundsList.push(bSound);
+    }
+    if (lettersList[i] === "c"|| lettersList[i] === "C"){
+      soundsList.push(cSound);
+    }
+    if (lettersList[i] === "d"|| lettersList[i] === "D"){
+      soundsList.push(dSound);
+    }
+    if (lettersList[i] === "e"|| lettersList[i] === "E"){
+      soundsList.push(eSound);
+    }
+    if (lettersList[i] === "f"|| lettersList[i] === "F"){
+      soundsList.push(fSound);
+    }
+    if (lettersList[i] === "g"|| lettersList[i] === "G"){
+      soundsList.push(gSound);
+    }
+    if (lettersList[i] === "h"|| lettersList[i] === "H"){
+      soundsList.push(hSound);
+    }
+    if (lettersList[i] === "i"|| lettersList[i] === "I"){
+      soundsList.push(iSound);
+    }
+    if (lettersList[i] === "j"|| lettersList[i] === "J"){
+      soundsList.push(jSound);
+    }
+    if (lettersList[i] === "k"|| lettersList[i] === "K"){
+      soundsList.push(kSound);
+    }
+    if (lettersList[i] === "l"|| lettersList[i] === "L"){
+      soundsList.push(lSound);
+    }
+    if (lettersList[i] === "m"|| lettersList[i] === "M"){
+      soundsList.push(mSound);
+    }
+    if (lettersList[i] === "n"|| lettersList[i] === "N"){
+      soundsList.push(nSound);
+    }
+    if (lettersList[i] === "o"|| lettersList[i] === "O"){
+      soundsList.push(oSound);
+    }
+    if (lettersList[i] === "p"|| lettersList[i] === "P"){
+      soundsList.push(pSound);
+    }
+    if (lettersList[i] === "q"|| lettersList[i] === "Q"){
+      soundsList.push(qSound);
+    }
+    if (lettersList[i] === "r"|| lettersList[i] === "R"){
+      soundsList.push(rSound);
+    }
+    if (lettersList[i] === "s"|| lettersList[i] === "S"){
+      soundsList.push(sSound);
+    }
+    if (lettersList[i] === "t"|| lettersList[i] === "T"){
+      soundsList.push(tSound);
+    }
+    if (lettersList[i] === "u"|| lettersList[i] === "U"){
+      soundsList.push(uSound);
+    }
+    if (lettersList[i] === "v"|| lettersList[i] === "V"){
+      soundsList.push(vSound);
+    }
+    if (lettersList[i] === "w"|| lettersList[i] === "W"){
+      soundsList.push(wSound);
+    }
+    if (lettersList[i] === "x"|| lettersList[i] === "X"){
+      soundsList.push(xSound);
+    }
+    if (lettersList[i] === "y"|| lettersList[i] === "Y"){
+      soundsList.push(ySound);
+    }
+    if (lettersList[i] === "z"|| lettersList[i] === "Z"){
+      soundsList.push(zSound);
     }
   }
+}
+
+function playsound(){
+  soundsList.play();
 }
