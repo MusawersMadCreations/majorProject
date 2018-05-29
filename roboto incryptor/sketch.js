9fef5381d5541b7060306c49b616372bca2b844f
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-05-29 15:20:32 -0600

    wdaw

--- roboto incryptor/sketch.js
+++ roboto incryptor/sketch.js
@@ -1,11 +1,10 @@
-
 // Roboto Language incryptor
 // Musawer Jalal
 // May 9, 2018
 
 // global variables
-let aSound,bSound,cSound,dSound,eSound,fSound,gSound,hSound,iSound,jSound,kSound,lSound,mSound,nSound;
-let oSound,pSound,qSound,rSound,sSound,tSound,uSound,vSound,wSound,xSound,ySound,zSound,spaceSound;
+let aSound, bSound, cSound, dSound, eSound, fSound, gSound, hSound, iSound, jSound, kSound, lSound, mSound, nSound;
+let oSound, pSound, qSound, rSound, sSound, tSound, uSound, vSound, wSound, xSound, ySound, zSound, spaceSound;
 let textbox;
 let submitButton, playSoundButton;
 
@@ -14,20 +13,23 @@ let soundsList = [];
 
 let myTimer;
 
+let wordtext = {
+  x: 100,
+  y: 100,
+};
+
+
 function preload() {
-  aSound = loadSound("sounds/A.wav"),bSound = loadSound("sounds/B.wav"),cSound = loadSound("sounds/C.wav"),dSound = loadSound("sounds/D.wav"),eSound = loadSound("sounds/E.wav");
-  fSound = loadSound("sounds/F.wav"),gSound = loadSound("sounds/G.wav"),hSound = loadSound("sounds/H.wav"),iSound = loadSound("sounds/I.wav"),jSound = loadSound("sounds/J.wav");
-  kSound = loadSound("sounds/K.wav"),lSound = loadSound("sounds/L.wav"),mSound = loadSound("sounds/M.wav"),nSound = loadSound("sounds/N.wav"),oSound = loadSound("sounds/O.wav");
-  pSound = loadSound("sounds/P.wav"),qSound = loadSound("sounds/Q.wav"),rSound = loadSound("sounds/R.wav"),sSound = loadSound("sounds/S.wav"),tSound = loadSound("sounds/T.wav");
-  uSound = loadSound("sounds/U.wav"),vSound = loadSound("sounds/V.wav"),wSound = loadSound("sounds/W.wav"),xSound = loadSound("sounds/X.wav"),ySound = loadSound("sounds/Y.wav");
+  aSound = loadSound("sounds/A.wav"), bSound = loadSound("sounds/B.wav"), cSound = loadSound("sounds/C.wav"), dSound = loadSound("sounds/D.wav"), eSound = loadSound("sounds/E.wav");
+  fSound = loadSound("sounds/F.wav"), gSound = loadSound("sounds/G.wav"), hSound = loadSound("sounds/H.wav"), iSound = loadSound("sounds/I.wav"), jSound = loadSound("sounds/J.wav");
+  kSound = loadSound("sounds/K.wav"), lSound = loadSound("sounds/L.wav"), mSound = loadSound("sounds/M.wav"), nSound = loadSound("sounds/N.wav"), oSound = loadSound("sounds/O.wav");
+  pSound = loadSound("sounds/P.wav"), qSound = loadSound("sounds/Q.wav"), rSound = loadSound("sounds/R.wav"), sSound = loadSound("sounds/S.wav"), tSound = loadSound("sounds/T.wav");
+  uSound = loadSound("sounds/U.wav"), vSound = loadSound("sounds/V.wav"), wSound = loadSound("sounds/W.wav"), xSound = loadSound("sounds/X.wav"), ySound = loadSound("sounds/Y.wav");
   zSound = loadSound("sounds/Z.wav"), spaceSound = loadSound("sounds/space.wav");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-
-
 
   fSound.play();
   // creates text input box
@@ -42,128 +44,128 @@ function setup() {
 
 
   changeTextToSound();
-  textbox.changed(recordInput);   // records text
+  textbox.changed(recordInput); // records text
 
 
 
 }
 
 function draw() {
-  background(255);
-  playSoundButton.mousePressed(playsound);
+  checkEvents();
+
 }
 
-function recordInput(){
+function checkEvents() {
+  playSoundButton.mousePressed(playsound);
+  submitButton.mousePressed(submitText);
+}
+
+function recordInput() {
   lettersList = [];
   soundsList = [];
   lettersList = textbox.value().split("");
   console.log(lettersList);
   changeTextToSound();
-
-
 }
 
 
-function changeTextToSound(){
+function changeTextToSound() {
   for (let i = 0; i < lettersList.length; i++) {
-    if (lettersList[i] === "a" || lettersList[i] === "A"){
+    if (lettersList[i] === "a" || lettersList[i] === "A") {
       soundsList.push(aSound);
     }
-    if (lettersList[i] === "b"|| lettersList[i] === "B"){
+    if (lettersList[i] === "b" || lettersList[i] === "B") {
       soundsList.push(bSound);
     }
-    if (lettersList[i] === "c"|| lettersList[i] === "C"){
+    if (lettersList[i] === "c" || lettersList[i] === "C") {
       soundsList.push(cSound);
     }
-    if (lettersList[i] === "d"|| lettersList[i] === "D"){
+    if (lettersList[i] === "d" || lettersList[i] === "D") {
       soundsList.push(dSound);
     }
-    if (lettersList[i] === "e"|| lettersList[i] === "E"){
+    if (lettersList[i] === "e" || lettersList[i] === "E") {
       soundsList.push(eSound);
     }
-    if (lettersList[i] === "f"|| lettersList[i] === "F"){
+    if (lettersList[i] === "f" || lettersList[i] === "F") {
       soundsList.push(fSound);
     }
-    if (lettersList[i] === "g"|| lettersList[i] === "G"){
+    if (lettersList[i] === "g" || lettersList[i] === "G") {
       soundsList.push(gSound);
     }
-    if (lettersList[i] === "h"|| lettersList[i] === "H"){
+    if (lettersList[i] === "h" || lettersList[i] === "H") {
       soundsList.push(hSound);
     }
-    if (lettersList[i] === "i"|| lettersList[i] === "I"){
+    if (lettersList[i] === "i" || lettersList[i] === "I") {
       soundsList.push(iSound);
     }
-    if (lettersList[i] === "j"|| lettersList[i] === "J"){
+    if (lettersList[i] === "j" || lettersList[i] === "J") {
       soundsList.push(jSound);
     }
-    if (lettersList[i] === "k"|| lettersList[i] === "K"){
+    if (lettersList[i] === "k" || lettersList[i] === "K") {
       soundsList.push(kSound);
     }
-    if (lettersList[i] === "l"|| lettersList[i] === "L"){
+    if (lettersList[i] === "l" || lettersList[i] === "L") {
       soundsList.push(lSound);
     }
-    if (lettersList[i] === "m"|| lettersList[i] === "M"){
+    if (lettersList[i] === "m" || lettersList[i] === "M") {
       soundsList.push(mSound);
     }
-    if (lettersList[i] === "n"|| lettersList[i] === "N"){
+    if (lettersList[i] === "n" || lettersList[i] === "N") {
       soundsList.push(nSound);
     }
-    if (lettersList[i] === "o"|| lettersList[i] === "O"){
+    if (lettersList[i] === "o" || lettersList[i] === "O") {
       soundsList.push(oSound);
     }
-    if (lettersList[i] === "p"|| lettersList[i] === "P"){
+    if (lettersList[i] === "p" || lettersList[i] === "P") {
       soundsList.push(pSound);
     }
-    if (lettersList[i] === "q"|| lettersList[i] === "Q"){
+    if (lettersList[i] === "q" || lettersList[i] === "Q") {
       soundsList.push(qSound);
     }
-    if (lettersList[i] === "r"|| lettersList[i] === "R"){
+    if (lettersList[i] === "r" || lettersList[i] === "R") {
       soundsList.push(rSound);
     }
-    if (lettersList[i] === "s"|| lettersList[i] === "S"){
+    if (lettersList[i] === "s" || lettersList[i] === "S") {
       soundsList.push(sSound);
     }
-    if (lettersList[i] === "t"|| lettersList[i] === "T"){
+    if (lettersList[i] === "t" || lettersList[i] === "T") {
       soundsList.push(tSound);
     }
-    if (lettersList[i] === "u"|| lettersList[i] === "U"){
+    if (lettersList[i] === "u" || lettersList[i] === "U") {
       soundsList.push(uSound);
     }
-    if (lettersList[i] === "v"|| lettersList[i] === "V"){
+    if (lettersList[i] === "v" || lettersList[i] === "V") {
       soundsList.push(vSound);
     }
-    if (lettersList[i] === "w"|| lettersList[i] === "W"){
+    if (lettersList[i] === "w" || lettersList[i] === "W") {
       soundsList.push(wSound);
     }
-    if (lettersList[i] === "x"|| lettersList[i] === "X"){
+    if (lettersList[i] === "x" || lettersList[i] === "X") {
       soundsList.push(xSound);
     }
-    if (lettersList[i] === "y"|| lettersList[i] === "Y"){
+    if (lettersList[i] === "y" || lettersList[i] === "Y") {
       soundsList.push(ySound);
     }
-    if (lettersList[i] === "z"|| lettersList[i] === "Z"){
+    if (lettersList[i] === "z" || lettersList[i] === "Z") {
       soundsList.push(zSound);
     }
-    if (lettersList[i] === " "){
-      soundsList.push(spaceSound);
-    }
-    else{
+    if (lettersList[i] === " ") {
       soundsList.push(spaceSound);
     }
   }
 }
 
-function playsound(){
-  for (let i = 0; i < soundsList.length; i++) {
+function playsound() {
+  if (soundsList.length >= 1) {
     myTimer = new Timer(200);
-    if (myTimer.isDone()) {
-      soundsList[i].play();
-      myTimer.reset();
+    for (let i = 0; i < soundsList.length; i++) {
+      if (myTimer.isDone()) {
+        soundsList[i].play();
+        myTimer.reset(200);
+      }
     }
-
   }
 }
-
 class Timer {
   constructor(waitTime) {
     this.waitTime = waitTime;
@@ -182,9 +184,19 @@ class Timer {
   isDone() {
     if (millis() >= this.finishTime) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
+  }
+}
+
+function submitText() {
+  textSize(100);
+  fill(0);
+  if (lettersList.length < 1) {
+    text("You Can't Submit Nothing", wordtext.x, wordtext.y);
+    print('ha');
+  } else {
+    text("Your Text Has Been EnCrypted", wordtext.x, wordtext.y);
   }
 }
