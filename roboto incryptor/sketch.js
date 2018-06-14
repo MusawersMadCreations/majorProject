dc9e84dfc951d40559081d020bdb5251af220421
MusawersMadCreations <36053939+MusawersMadCreations@users.noreply.github.com>
2018-06-14 15:22:16 -0600

    34324

--- roboto incryptor/sketch.js
+++ roboto incryptor/sketch.js
@@ -199,13 +199,44 @@ function playsound() {
   if (state === "program") {
     if (soundsList.length >= 1) {
       background(255);
-      for (let i = 0; i < soundsList.length; i++) {
-        soundsList[i].play();
+      myTimer = new Timer(500);
+      let i = 0;
+      while (i < lettersList.length) {
+        lettersList[i].play();
+        if (myTimer.isDone()) {
+          i++;
+          myTimer.reset(500);
+        }
       }
     }
   }
 }
 
+
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
 
 class Letter {
   constructor(x, y, size, speed, color) {
